a1656cb65153265f01874a31a4f6c3afc594694b
Wes Hargrove <1852758+whargrove@users.noreply.github.com>
2024-01-29 22:28:19 -0700

    Trim to Gradle (#1)

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,11 +1,7 @@
 package demo;
 
-import lombok.Generated;
-
 import static java.lang.System.out;
 
-/** Production class for integration test demonstration. */
-@Generated // Lie to JaCoCo and PIT
 public final class Application {
     /**
      * Main entry point.
@@ -13,9 +9,9 @@ public final class Application {
      * @param args the command-line arguments
      */
     public static void main(final String... args) {
-        out.println(new TheFoo("I AM FOOCUTUS OF BORG"));
+        out.println("Hello, World!");
     }
 
     // This is a "utility" class, ie, no instances, only static methods
-    private Application() { }
+    private Application() {}
 }
---
+++ src/test/java/demo/TestApplication.java
@@ -0,0 +1,31 @@
+package demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestApplication {
+
+    private final PrintStream stdOut = System.out;
+    private final ByteArrayOutputStream outCapture = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void beforeEach() {
+        System.setOut(new PrintStream(outCapture));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(stdOut);
+    }
+
+    @Test
+    void shouldRun() {
+        Application.main(new String[0]);
+        assertThat(outCapture.toString()).contains("Hello, World!");
+    }
+}
